9857d9e17b2b25fa47840681c80fc8a72350fdf6
Desmond Naranjo <dhnaranjo@gmail.com>
2018-03-30 11:36:59 -0500

    Extracted tables to partials and model logic

--- app/models/person.rb
+++ app/models/person.rb
@@ -5,8 +5,14 @@ class Person < ActiveRecord::Base
   has_many :user_person_joins
   has_many :users, through: :user_person_joins
 
-  def up_by(event=nil)
-    return attributes['up_by'] unless event
+  def self.ordered_by_up_by(people, event = nil)
+    people
+      .sort_by { |person| person.up_by(event).to_i }
+      .reverse
+  end
+
+  def up_by(event = nil)
+    return attributes['up_by'] if event.nil?
     checkins_from_event = event.checkins.where(person: self).order(:created_at)
     first_checkin = checkins_from_event.first
     last_checkin = checkins_from_event.last
