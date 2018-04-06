204c41bf79908348b99243b56ea232a5f7123cdf
Desmond Naranjo <dhnaranjo@gmail.com>
2018-04-06 16:15:39 -0500

    Consistent db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,45 +1,47 @@
 require 'csv'
 
-participants_csv = CSV.foreach(Rails.root.join('lib/seeds/participants.csv'), headers: true)
-
-date_sorted_participants = participants_csv
+participants_csv = CSV
+  .foreach(Rails.root.join('lib/seeds/participants.csv'), headers: true)
   .to_a
-  .sort_by { |row| Date.parse(row['Date']) }
 
-people = date_sorted_participants.uniq { |row| row['Name'] }
+leagues = participants_csv.uniq { |row| row['League'] }.map do |league|
+  League.new(
+    name: league['League'],
+    created_at: league['Date']
+  )
+end
+
+people = participants_csv.uniq { |row| row['Name'] }.map do |person|
+  Person.new(
+    name: person['Name'],
+    created_at: person['Date'],
+    league: leagues.select { |league| league.name == person['League'] }.first
+  )
+end
+
+events = participants_csv.uniq { |row| row['Event'] }.map do |event|
+  Event.new(
+    name: event['Event'],
+    created_at: event['Date']
+  )
+end
+
 Person.transaction do
-  people.each do |person|
-    Person.create(
-      name: person['Name'],
-      created_at: person['Date'],
-      league: League.find_or_initialize_by(
-        name: person['League'],
-        created_at: person['Date']
-      )
-    )
-  end
+  leagues.each(&:save!)
+  people.each(&:save!)
+  events.each(&:save!)
 end
 
-events = date_sorted_participants.uniq { |event| event['Event'] }
-Event.transaction do
-  events.each do |event|
-    Event.create(name: event['Event'], created_at: event['Date'])
-  end
-end
-
-checkins_csv = CSV.foreach(Rails.root.join('lib/seeds/weighins.csv'), headers: true)
-
-time_sorted_checkins = checkins_csv
+checkins = CSV.foreach(Rails.root.join('lib/seeds/weighins.csv'), headers: true)
   .to_a
-  .sort_by { |row| Date.parse(row['Time']) }
-
-Checkin.transaction do
-  time_sorted_checkins.each do |checkin|
+  .sort_by { |row| [DateTime.parse(row['Time']), row['Name']] }
+  .each do |checkin|
     CreateCheckin.call(
-      Person.find_by(name: checkin['Name']),
-      Event.find_by('DATE(created_at) = ?', Date.parse(checkin['Time'])),
+      # Person.find_by(name: checkin['Name']),
+      people.select { |person| person.name == checkin['Name'] }.first,
+      events.select { |event| event.created_at.to_date == Date.parse(checkin['Time']) }.first,
+      # Event.find_by('DATE(created_at) = ?', Date.parse(checkin['Time'])),
       checkin['Weight'].to_f,
       nil
     )
-  end
 end
